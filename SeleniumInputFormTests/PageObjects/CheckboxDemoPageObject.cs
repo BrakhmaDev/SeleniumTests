0875e8765e88b1513a4c3eefc1dc27981f058cfe
Marat <ounce836475@mail.ru>
2021-04-18 23:04:12 +0300

    Add cycle fo Checkboxes

--- SeleniumInputFormTests/PageObjects/CheckboxDemoPageObject.cs
+++ SeleniumInputFormTests/PageObjects/CheckboxDemoPageObject.cs
@@ -12,7 +12,9 @@ namespace SeleniumInputFormTests.PageObjects
         private IWebDriver webDriver;
         private readonly By checkboxDemoButton = By.XPath("//li[@style='display: list-item;']/child::a[text()[contains(.,'Checkbox Demo')]]");
         private readonly By clickOnthisCheck_box = By.XPath("//div[@class='checkbox']/child::label[text()[contains(.,'Click on this check box')]]");
-
+        private readonly By successMessage = By.XPath("//div[text()[contains(.,'Success - Check box is checked')]]");
+        private readonly By checkAllButton = By.XPath("//input[@value='Check All']");
+        private readonly By uncheckAllButton = By.XPath("//input[@value='Uncheck All']");       
         public CheckboxDemoPageObject(IWebDriver webDriver)
         {
             this.webDriver = webDriver;
@@ -22,6 +24,14 @@ namespace SeleniumInputFormTests.PageObjects
             webDriver.FindElement(checkboxDemoButton).Click();
             WaitUntil.WaitElement(webDriver, clickOnthisCheck_box);
             webDriver.FindElement(clickOnthisCheck_box).Click();
+            WaitUntil.WaitElement(webDriver, successMessage);
+            webDriver.FindElement(checkAllButton).Click();
+            WaitUntil.WaitElement(webDriver, uncheckAllButton);
+            for (int i = 1; i < 5; i++)
+            {
+                webDriver.FindElement(By.XPath($"//label[text()[contains(.,'Option {i}')]]")).Click();
+            }
+            WaitUntil.WaitElement(webDriver, checkAllButton);
             return new MainMenuPageObject(webDriver);
         }
     }
